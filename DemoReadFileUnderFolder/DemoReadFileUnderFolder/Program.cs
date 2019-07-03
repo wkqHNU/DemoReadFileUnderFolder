e3e2a1c6840dcaeea7267593bbfb3b6dc56947ab
wkq <172137365@qq.com>
2019-07-03 09:09:21 +0800

    遍历文件夹下子文件夹下的文件

---
+++ DemoReadFileUnderFolder/DemoReadFileUnderFolder/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoReadFileUnderFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScanPatternFiles();
+        }
+
+        public static List<string[]> ScanPatternFiles()
+        {
+            try
+            {
+                string antPatDir = @"..\..\..\GainAntennaPattern\";
+                string paPath;
+                string saPath;
+                List<string[]> antPatLst = new List<string[]>();
+                DirectoryInfo di = new DirectoryInfo(antPatDir);
+                DirectoryInfo[] subs = di.GetDirectories();
+                foreach (var dir in subs)
+                {
+                    try
+                    {
+                        paPath = dir.GetFiles("*AIR0.txt")[0].FullName;
+                        saPath = dir.GetFiles("*AIR1.txt")[0].FullName;
+                        antPatLst.Add(new string[] { paPath, saPath });
+                    }
+                    catch { continue; }  //有问题的方向图组直接跳过
+                }
+                return antPatLst;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to scan patterns.", ex);
+                return null;
+            }
+        }
+    }
+}
